df2e2a3738d0d759d6bd3e54cfcb9da0676a69d6
Surge Sursus <accounts@risen.community>
2022-01-19 23:03:58 -0500

    build

--- src/App.js
+++ src/App.js
@@ -34,7 +34,12 @@ function App() {
         faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
         faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
         faceapi.nets.faceExpressionNet.loadFromUri('/models')
-      ]).then(handleVideo)
+      ]).then(() => {
+        handleVideo()
+        setTimeout(() => {
+          setFaces('Look Surprised')
+        }, 2000)
+      })
     }
     videoRef.current && loadModels()
   }, [])
@@ -44,9 +49,7 @@ function App() {
   }
 
   /*
-        setTimeout(() => {
-          setFaces('Look Surprised')
-        }, 2000)
+        
   */
 
   return (
