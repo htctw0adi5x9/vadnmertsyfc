9ba1075dd7e97142f8726758fa25ef8149f66c46
Surge Sursus <accounts@risen.community>
2022-01-19 22:56:54 -0500

    build

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   const videoRef = useRef();
   const mediaStream = useUserMedia(CAPTURE_OPTIONS);
   const [faces, setFaces] = useState('Place Face in Frame') // Happy, Neutral
-  const [text, setText] = useState([])
 
   if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
     videoRef.current.srcObject = mediaStream;
@@ -21,10 +20,11 @@ function App() {
 
   const handleVideo = async () => {
     const detections = await faceapi
-    .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
-    .withFaceLandmarks()
-    .withFaceExpressions()
-    setText(detections)
+      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions()
+
+    console.log(detections)
   }
 
   useEffect(() => {
@@ -34,7 +34,7 @@ function App() {
         faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
         faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
         faceapi.nets.faceExpressionNet.loadFromUri('/models')
-      ]).then(handleVideo())
+      ]).then(handleVideo)
     }
     videoRef.current && loadModels()
   }, [])
@@ -52,7 +52,6 @@ function App() {
   return (
     <div style={{height: '100%', width: '100%', backgroundColor: 'black'}}>
       <div style={{color: 'white', fontSize: 20, textAlign: 'center', padding: 15}}>{faces}</div>
-      <div style={{color: 'white', fontSize: 18}}>{'Detections:' + text}</div>
       <video id='video' style={{position: 'fixed', height: '100%', width: '100%', objectFit: 'cover'}} ref={videoRef} onCanPlay={handleCanPlay} autoPlay playsInline muted />
       <div id='container'>
         <img id='scan' style={{height: '80vw', width: '80vw', opacity: .15}} src={faceid} alt="Logo" />
