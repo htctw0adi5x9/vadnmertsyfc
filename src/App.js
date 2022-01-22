b2cdddc1d9300563139c65fed2e82bae0dd559a5
Surge Sursus <accounts@risen.community>
2022-01-22 16:23:46 -0500

    build

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App({ onCapture, onClear }) {
         <div style={{position: 'absolute', bottom: 70, left: '2.5vw'}}>
           <Canvas
             ref={canvasRef}
-            width={(w / 4) + 10}
+            width={(w / 4) + 15}
             height={h / 4}
             style={{objectFit:'cover'}}
           />
